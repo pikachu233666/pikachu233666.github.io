af9f1db0d1168e3c95d9615ba5818e342f30b74e
pikachu233666 <2586970609@qq.com>
2023-01-10 16:24:46 +0800

    update

---
+++ uptime-status/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1900384-3640b4be0582c5e5037ae83d'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.pochacco.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/pikachu233666/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://pochacco.top/'
+    },
+  ],
+};
